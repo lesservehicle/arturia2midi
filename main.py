672c24ca6ad90f00ff36f6f33bf8ff3026439278
Adam Copeland <adam@lesservehicle.com>
2024-03-26 12:00:25 -0400

    Extra output and documentation

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from json_repair import repair_json
 def load_json_file(file_path):
     with open(file_path, "r") as file:
         json_string = file.read()
+        
+        # Using repair_json library here because the default output from Arturia is 
+        # an incorrectly formatted JSON file
         return json.loads(repair_json(json_string))
 
 
@@ -51,6 +54,7 @@ def decode_project_file(project_file_path, parameters_file_path):
     tempo_mdsb = None
     tempo_msb = None
     tempo = None
+    swing = None
 
     # Iterate over each item in the project file
     for key, value in project_data.items():
@@ -74,8 +78,25 @@ def decode_project_file(project_file_path, parameters_file_path):
         if track == "device":
             decoded_project["device"][key] = value
             continue
+        
+        """ 
+            Decoding Tempo
+            
+            Tempo is determined by combining three fields for least-significant bits (LSB), middle significance bits (MidSB)
+            and most significant bits (MSB). For example, a tempo of 120.00 is represented in the three fields as follows:
+            
+                  Key: Value                    Binary
+                - 120_70: 96    (MSB)       ->  1100000
+                - 120_71: 93    (MidSB)     ->  1011101
+                - 120_72: 0     (LSB)       ->  0000000
+                
+                Combined binary             ->  110000010111010000000
+                In decimal                  ->  12000
+                / 100 for Tempo             ->  120.00
+        """
+        
         if track == "project" and tempo is None:
-            # Decode Tempo
+            
             if key == "120_70":
                 # Project tempo part 3 (LSB)
                 tempo_lsb = bin(value)
@@ -92,7 +113,30 @@ def decode_project_file(project_file_path, parameters_file_path):
                 decoded_project[track]['tempo'] = tempo
                 print(f"Detected Tempo: {tempo}")
                 continue
-            
+          
+        # Find swing parameter  
+        if track == "project" and swing is None:
+            if key == "120_74":
+                swing = value
+                decoded_project[track]['swing'] = swing
+                continue        
+        
+        """
+        Determine if Track 1 is in SEQ or DRUM mode:
+
+        This parameter is set per pattern, but a limitation in MIDI allows us to only set one instrument type per track.
+        Therefore, we'll derive the mode from the first pattern in the track.
+
+        The value for SEQ/DRUM mode is derived from the first bit in the value of Parameter ID 100. For example, for 
+        Track 1 (123), Pattern 1 (1):
+                Key: Value              Binary
+            -   123_100_1: 26       ->  00011010
+            -   
+        """
+        
+        if track == "track1":
+            if key == "123_100_1":
+                
         elif track == "unknown":
             continue
 
@@ -114,8 +158,7 @@ def decode_project_file(project_file_path, parameters_file_path):
 
         # Parse the param_id, there always should be one
         if param_id not in decoded_project[track]:
-            decoded_project[track][param_id] = {}
-
+            decoded_project[track][param_id] = {} 
         if pattern_id not in decoded_project[track][param_id]:
             decoded_project[track][param_id][pattern_id] = {}
         if multiplier not in decoded_project[track][param_id][pattern_id]:
@@ -123,27 +166,45 @@ def decode_project_file(project_file_path, parameters_file_path):
         if note_id not in decoded_project[track][param_id][pattern_id][multiplier]:
             decoded_project[track][param_id][pattern_id][multiplier][note_id] = {}
 
-        decoded_project[track][param_id][pattern_id][multiplier][note_id] = value
+        decoded_project[track][param_id][pattern_id][multiplier][note_id]['value'] = value
+        decoded_project[track][param_id][pattern_id][multiplier][note_id]['key'] = key
+        decoded_project[track][param_id][pattern_id][multiplier][note_id]['decoded_key'] = decoded_key
 
     return decoded_project
 
 
 def to_midi_track(midi_data, decoded_track, track, is_drum):
 
+    # Grab the Project's tempo parameter that we derived earlier
     bpm = decoded_track['project']['tempo']
+    
+    # Copy just the information for a specific track
     decoded_track = decoded_track[track]
 
+    # It's important to determine if Track 1 is DRUM or SEQ, because the Parameter IDs
+    # that we need to fetch are going to be different based on that.
+
     if is_drum:
+        # Parameter ID 54:  DRUM step corresponding step
         steps = decoded_track["54"]
+        # Parameter ID 117: DRUM note pitch
         pitches = decoded_track["117"]
+        # Parameter ID 118: DRUM note gate length 
         gates = decoded_track["118"]
+        # Parameter ID 119: DRUM note velocity
         velocities = decoded_track["119"]
     else:
+        # Parameter ID 50:  Seq note corresponding step
         steps = decoded_track["50"]
+        # Parameter ID 109: Seq note pitch
         pitches = decoded_track["109"]
+        # Parameter ID 110: Seq note gate length
         gates = decoded_track["110"]
+        # Parameter ID 111: Seq note velocity
         velocities = decoded_track["111"]
 
+    # We have to derive the active steps from Parameter ID 54 or 50 before we can iterate on them
+    # and fill them in with pitch, gate, and velocity
     track_steps = {}
 
     # Calculate the steps with actual notes on them in the pattern
@@ -155,36 +216,49 @@ def to_midi_track(midi_data, decoded_track, track, is_drum):
             if mult == "1":
 
                 for note, step in subdata.items():
-                    if step != 127:
-                        if int(step) not in pattern_note_steps:
-                            pattern_note_steps[int(step)] = {}
+                    if step['value'] != 127:
+                        if int(step['value']) not in pattern_note_steps:
+                            pattern_note_steps[int(step['value'])] = {}
 
-                        pattern_note_steps[int(step)][note] = {
-                            "pitch": int(pitches[pattern][mult][note]),
-                            "velocity": int(velocities[pattern][mult][note]),
-                            "gate": int(gates[pattern][mult][note]),
+                        pattern_note_steps[int(step['value'])][note] = {
+                            "pitch": int(pitches[pattern][mult][note]['value']),
+                            "velocity": int(velocities[pattern][mult][note]['value']),
+                            "gate": int(gates[pattern][mult][note]['value']),
                         }
 
         track_steps[int(pattern)] = pattern_note_steps
 
+    # It helps later to sort the track data by pattern ID
     track_data = dict(sorted(track_steps.items()))
 
-    # Create an Instrument instance (using a piano here, but you can choose any)
+    # Create an Instrument instance
+    # I'd like to offer support for changing this based on user choice
     if is_drum:
-        instrument = pretty_midi.Instrument(is_drum=is_drum, program=0)
+        instrument = pretty_midi.Instrument(is_drum=is_drum, program=pretty_midi.instrument_name_to_program("Drums"))
     else:
         instrument = pretty_midi.Instrument(
             program=pretty_midi.instrument_name_to_program("Acoustic Grand Piano")
         )
 
-    # Define the duration of a 1/16th note in seconds (assuming 120 BPM for simplicity)
-    # Use parameters from the file to figure out tempo later
+    # Set the sixteenth note length based on the tempo we derived from the project
     sixteenth_note_duration = 60 / (bpm * 4)
+    
+    # Keep a cursor so we know when the current pattern ends
     pattern_cursor = 0
     
-    # Process each note in the dictionary
+    # For a list of patterns with data in them
+    pattern_list = []
+
+    # Loop through the patterns
     for pattern, step_info in track_data.items():
+        
+        # Add the pattern ID to the list of patterns with data in them
+        if step_info != {}:
+            pattern_list.append(pattern)
+        
+        # Loop through the step info in the patterns
         for step, note_info in step_info.items():
+            # Loop through the note info associated with the steps
             for note_id, attrs in note_info.items():
                 # Calculate the start time for the note (step * duration of a sixteenth note)
                 start_time = step * sixteenth_note_duration * 2 + pattern_cursor * sixteenth_note_duration * 2
@@ -202,10 +276,15 @@ def to_midi_track(midi_data, decoded_track, track, is_drum):
 
                 # Add the note to the instrument
                 instrument.notes.append(note)
-        pattern_cursor = pattern_cursor + 64
                 
+        # Increment the cursor by pattern length (defaulting to 64 for now until I can decode pattern length)
+        pattern_cursor = pattern_cursor + 64
+    
+    print(f"Patterns in {track}: {pattern_list}")
+    
     # Add the instrument to the PrettyMIDI object
     midi_data.instruments.append(instrument)
+    
     return midi_data
 
 
@@ -220,7 +299,9 @@ if __name__ == "__main__":
 
     # Paths to your project and parameters (preferences) files
     project_file_path = f"Projects/{args.project}.{args.instrument}"
+    print(f"Reading project file {project_file_path}")
     parameters_file_path = f"Instruments/{args.instrument}.json"
+    print(f"Reading parameters file {parameters_file_path}")
 
     # Decode the project file
     decoded_project_data = decode_project_file(project_file_path, parameters_file_path)
@@ -228,6 +309,7 @@ if __name__ == "__main__":
     # Initialize PrettyMIDI object
     midi_data = pretty_midi.PrettyMIDI()
     for i in range(1, 5):
+        print(f"Converting Track {i} to MIDI")
         if i == 1:
             is_drum = True
         else:
@@ -243,3 +325,5 @@ if __name__ == "__main__":
         json_out = json.dumps(decoded_project_data, indent=4, sort_keys=True)
         with open("output.json", "w") as fp:
             fp.write(json_out)
+
+    print(f"Decoded project written to {args.json}")
