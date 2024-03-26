78657f1c0413267169272166c3f224ca74fbfa6c
Adam Copeland <adam@lesservehicle.com>
2024-03-26 13:02:18 -0400

    huge documentation update

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import re
 import argparse
 import pretty_midi
 from json_repair import repair_json
-
-
+from pprint import pprint
+from tomark import Tomark
 
 # Function to load a JSON file
 def load_json_file(file_path):
     with open(file_path, "r") as file:
         json_string = file.read()
-        
-        # Using repair_json library here because the default output from Arturia is 
+
+        # Using repair_json library here because the default output from Arturia is
         # an incorrectly formatted JSON file
         return json.loads(repair_json(json_string))
 
@@ -49,7 +49,7 @@ def decode_project_file(project_file_path, parameters_file_path):
         "track3": {},
         "track4": {},
     }
-    
+
     tempo_lsb = None
     tempo_mdsb = None
     tempo_msb = None
@@ -58,7 +58,7 @@ def decode_project_file(project_file_path, parameters_file_path):
 
     # Iterate over each item in the project file
     for key, value in project_data.items():
-        
+
         sections = key.split("_")
         track_id = sections[0]
 
@@ -78,7 +78,7 @@ def decode_project_file(project_file_path, parameters_file_path):
         if track == "device":
             decoded_project["device"][key] = value
             continue
-        
+
         """ 
             Decoding Tempo
             
@@ -94,9 +94,9 @@ def decode_project_file(project_file_path, parameters_file_path):
                 In decimal                  ->  12000
                 / 100 for Tempo             ->  120.00
         """
-        
+
         if track == "project" and tempo is None:
-            
+
             if key == "120_70":
                 # Project tempo part 3 (LSB)
                 tempo_lsb = bin(value)
@@ -106,24 +106,32 @@ def decode_project_file(project_file_path, parameters_file_path):
             elif key == "120_72":
                 # Project tempo part 1 (MSB)
                 tempo_msb = bin(value)
-                
-            if tempo_msb is not None and tempo_mdsb is not None and tempo_lsb is not None:
-                tempo = tempo_msb + tempo_mdsb.replace("0b", "") + tempo_lsb.replace("0b", "")
-                tempo = float(int(tempo, 2) / 100)   
-                decoded_project[track]['tempo'] = tempo
-                print(f"Detected Tempo: {tempo}")
+
+            if (
+                tempo_msb is not None
+                and tempo_mdsb is not None
+                and tempo_lsb is not None
+            ):
+                tempo = (
+                    tempo_msb
+                    + tempo_mdsb.replace("0b", "")
+                    + tempo_lsb.replace("0b", "")
+                )
+                tempo = float(int(tempo, 2) / 100)
+                decoded_project[track]["tempo"] = tempo
+                print(f"Detected Tempo: {tempo:.2f}")
                 continue
-          
-        # Find swing parameter  
+
+        # Find swing parameter
         if track == "project" and swing is None:
             if key == "120_74":
                 swing = value
-                decoded_project[track]['swing'] = swing
-                continue    
-            
+                decoded_project[track]["swing"] = swing
+                continue
+
         if track == "unknown":
-            continue    
-        
+            continue
+
         """
         TODO: Determine if Track 1 is in SEQ or DRUM mode:
 
@@ -154,7 +162,7 @@ def decode_project_file(project_file_path, parameters_file_path):
 
         # Parse the param_id, there always should be one
         if param_id not in decoded_project[track]:
-            decoded_project[track][param_id] = {} 
+            decoded_project[track][param_id] = {}
         if pattern_id not in decoded_project[track][param_id]:
             decoded_project[track][param_id][pattern_id] = {}
         if multiplier not in decoded_project[track][param_id][pattern_id]:
@@ -162,9 +170,11 @@ def decode_project_file(project_file_path, parameters_file_path):
         if note_id not in decoded_project[track][param_id][pattern_id][multiplier]:
             decoded_project[track][param_id][pattern_id][multiplier][note_id] = {}
 
-        decoded_project[track][param_id][pattern_id][multiplier][note_id]['value'] = value
-        decoded_project[track][param_id][pattern_id][multiplier][note_id]['key'] = key
-        decoded_project[track][param_id][pattern_id][multiplier][note_id]['decoded_key'] = decoded_key
+        decoded_project[track][param_id]["desc"] = decoded_key
+        decoded_project[track][param_id][pattern_id][multiplier][note_id][
+            "value"
+        ] = value
+        decoded_project[track][param_id][pattern_id][multiplier][note_id]["key"] = key
 
     return decoded_project
 
@@ -172,8 +182,8 @@ def decode_project_file(project_file_path, parameters_file_path):
 def to_midi_track(midi_data, decoded_track, track, is_drum):
 
     # Grab the Project's tempo parameter that we derived earlier
-    bpm = decoded_track['project']['tempo']
-    
+    bpm = decoded_track["project"]["tempo"]
+
     # Copy just the information for a specific track
     decoded_track = decoded_track[track]
 
@@ -185,7 +195,7 @@ def to_midi_track(midi_data, decoded_track, track, is_drum):
         steps = decoded_track["54"]
         # Parameter ID 117: DRUM note pitch
         pitches = decoded_track["117"]
-        # Parameter ID 118: DRUM note gate length 
+        # Parameter ID 118: DRUM note gate length
         gates = decoded_track["118"]
         # Parameter ID 119: DRUM note velocity
         velocities = decoded_track["119"]
@@ -206,23 +216,27 @@ def to_midi_track(midi_data, decoded_track, track, is_drum):
     # Calculate the steps with actual notes on them in the pattern
     for pattern, data in steps.items():
         pattern_note_steps = {}
+        if pattern != "desc":
+            for mult, subdata in data.items():
+                # Still need to figure out what the mult data is for, could be undo states
+                if mult == "1":
 
-        for mult, subdata in data.items():
-            # Still need to figure out what the mult data is for, could be undo states
-            if mult == "1":
+                    for note, step in subdata.items():
+                        if step["value"] != 127:
+                            if int(step["value"]) not in pattern_note_steps:
+                                pattern_note_steps[int(step["value"])] = {}
 
-                for note, step in subdata.items():
-                    if step['value'] != 127:
-                        if int(step['value']) not in pattern_note_steps:
-                            pattern_note_steps[int(step['value'])] = {}
+                            pattern_note_steps[int(step["value"])][note] = {
+                                "pitch": int(pitches[pattern][mult][note]["value"]),
+                                "velocity": int(
+                                    velocities[pattern][mult][note]["value"]
+                                ),
+                                "gate": int(gates[pattern][mult][note]["value"]),
+                            }
 
-                        pattern_note_steps[int(step['value'])][note] = {
-                            "pitch": int(pitches[pattern][mult][note]['value']),
-                            "velocity": int(velocities[pattern][mult][note]['value']),
-                            "gate": int(gates[pattern][mult][note]['value']),
-                        }
-
-        track_steps[int(pattern)] = pattern_note_steps
+            track_steps[int(pattern)] = pattern_note_steps
+        else:
+            continue
 
     # It helps later to sort the track data by pattern ID
     track_data = dict(sorted(track_steps.items()))
@@ -238,26 +252,29 @@ def to_midi_track(midi_data, decoded_track, track, is_drum):
 
     # Set the sixteenth note length based on the tempo we derived from the project
     sixteenth_note_duration = 60 / (bpm * 4)
-    
+
     # Keep a cursor so we know when the current pattern ends
     pattern_cursor = 0
-    
+
     # For a list of patterns with data in them
     pattern_list = []
 
     # Loop through the patterns
     for pattern, step_info in track_data.items():
-        
+
         # Add the pattern ID to the list of patterns with data in them
         if step_info != {}:
             pattern_list.append(pattern)
-        
+
         # Loop through the step info in the patterns
         for step, note_info in step_info.items():
             # Loop through the note info associated with the steps
             for note_id, attrs in note_info.items():
                 # Calculate the start time for the note (step * duration of a sixteenth note)
-                start_time = step * sixteenth_note_duration * 2 + pattern_cursor * sixteenth_note_duration * 2
+                start_time = (
+                    step * sixteenth_note_duration * 2
+                    + pattern_cursor * sixteenth_note_duration * 2
+                )
 
                 # Calculate the end time based on the gate value (assuming gate values are in sixteenth note durations)
                 end_time = start_time + attrs["gate"] / 2 * sixteenth_note_duration
@@ -272,54 +289,85 @@ def to_midi_track(midi_data, decoded_track, track, is_drum):
 
                 # Add the note to the instrument
                 instrument.notes.append(note)
-                
+
         # Increment the cursor by pattern length (defaulting to 64 for now until I can decode pattern length)
         pattern_cursor = pattern_cursor + 64
-    
+
     print(f"Patterns in {track}: {pattern_list}")
-    
+
     # Add the instrument to the PrettyMIDI object
     midi_data.instruments.append(instrument)
-    
+
     return midi_data
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--instrument", "-i", type=str, default="KeyStepPro")
-    parser.add_argument("--project", "-p", type=str, default="Sample")
-    parser.add_argument("--midi", "-m", type=str, default="Sample.mid")
-    parser.add_argument("--json", "-j", type=str)
+    parser.add_argument("--device", "-d", type=str, default="KeyStepPro", help="Coded name of the device to decode data for, eg. KeyStepPro")
+    parser.add_argument("--project", "-p", type=str, default="Sample", help="Name of a project to decode, eg. Sample")
+    parser.add_argument("--markdown", "-t", action='store_true', default=False, help="Print a markdown table of all Parameter IDs")
+    parser.add_argument("--midi", "-m", type=str, help="Enable MIDI export and provide path to output file, eg. Sample.mid")
+    parser.add_argument("--json", "-j", type=str, help="Enable JSON dump of decoded keys and provide path to output file, eg. Sample.json")
     args = parser.parse_args()
 
     # Paths to your project and parameters (preferences) files
-    project_file_path = f"Projects/{args.project}.{args.instrument}"
+    project_file_path = f"Projects/{args.project}.{args.device}"
     print(f"Reading project file {project_file_path}")
-    parameters_file_path = f"Instruments/{args.instrument}.json"
+    parameters_file_path = f"Devices/{args.device}.json"
     print(f"Reading parameters file {parameters_file_path}")
 
     # Decode the project file
     decoded_project_data = decode_project_file(project_file_path, parameters_file_path)
 
-    # Initialize PrettyMIDI object
-    midi_data = pretty_midi.PrettyMIDI()
-    for i in range(1, 5):
-        print(f"Converting Track {i} to MIDI")
-        if i == 1:
-            is_drum = True
-        else:
-            is_drum = False
-        midi_data = to_midi_track(midi_data, decoded_project_data, f"track{i}", is_drum)
+    if args.midi:
+        # Initialize PrettyMIDI object
+        midi_data = pretty_midi.PrettyMIDI()
+        for i in range(1, 5):
+            print(f"Converting Track {i} to MIDI")
+            if i == 1:
+                is_drum = True
+            else:
+                is_drum = False
+            midi_data = to_midi_track(midi_data, decoded_project_data, f"track{i}", is_drum)
 
-    # Write out the MIDI data to a new file
-    midi_data.write(args.midi)
-    print(f"MIDI written to {args.midi}")
-    
+        # Write out the MIDI data to a new file
+        midi_data.write(args.midi)
+        print(f"MIDI written to {args.midi}")
+
     # Output or process the decoded project data for debugs
     if args.json:
         json_out = json.dumps(decoded_project_data, indent=4, sort_keys=True)
         with open("output.json", "w") as fp:
             fp.write(json_out)
 
-    print(f"Decoded project written to {args.json}")
+        print(f"Decoded project written to {args.json}")
+
+
+    if args.markdown:
+        params = []
+
+        for track in ("device", "project", "track1", "track2", "track3", "track4"):
+
+            for param_id, value in decoded_project_data[track].items():
+                if track == "device" or (
+                    track == "project" and (param_id == "tempo" or param_id == "swing")
+                ):
+                    continue
+                elif "desc" in value:
+                    param = {}   
+                    param["id"] = int(param_id)                                 
+                    param["desc"] = value["desc"]
+                    params.append(param)
+        
+    
+        # dedup
+        params = [dict(t) for t in {tuple(d.items()) for d in params}]
+
+        # sort by ID
+        params = sorted(params, key=lambda d: d['id'])
+        
+        # convert to markdown table
+        markdown = Tomark.table(params)
+        print(markdown)
+    
--- readme.md
+++ readme.md
@@ -13,38 +13,156 @@ Converts Arturia MIDI Control Center project files to MIDI files. Please take no
 
 ## Installation
 
+For each iteration of this project, I run `pip freeze > requirements.txt` after testing to make sure I've got version-locked pip packages, so you should be able to just fire it up by installing `requirements.txt`.
+
 ```
 % pip install -r requirements.txt
 ```
 
+Of course, I would instead recommend that you use a Python Virtual Environment, preferably with version 3.11. I like to use [PyEnv](https://github.com/pyenv/pyenv) to simplify this process for me:
+
+```
+% pyenv install 3.11.5
+% pyenv virtualenv arturia2midi
+% pyenv activate arturia2midi
+% pip install -r requirements.txt
+```
+
+## File Formats for Devices and Projects
+
+Device files are available from the resources folder for MIDI Control Center. I've included a few of them in the repo here, under the `Devices` folder. To support a device, copy the (DeviceName).json file to the `Devices` folder and specify it with `-d (DeviceName)`, eg. `-p KeyStepPro`.
+
+Project files are exportable from MIDI Control Center, and typically have a file extension of `*.(DeviceName)`. For example, if I have a project called "FatBeats" on my KeyStep Pro and export it, the resultant file will be named `FatBeats.KeyStepPro`. Copy your project files into the `Projects` folder to enable them to be read and specify them with `-p (ProjectName)`, eg. `-p FatBeats`.
+
 ## Usage
+
 ```
 % python main.py -h
-
-usage: main.py [-h] [--instrument INSTRUMENT] [--project PROJECT] [--midi MIDI] [--json JSON]
+usage: main.py [-h] [--device DEVICE] [--project PROJECT] [--markdown] [--midi MIDI] [--json JSON]
 
 options:
   -h, --help            show this help message and exit
-  --instrument INSTRUMENT, -i INSTRUMENT
+  --device DEVICE, -d DEVICE
+                        Coded name of the device to decode data for, eg. KeyStepPro
   --project PROJECT, -p PROJECT
-  --midi MIDI, -m MIDI
-  --json JSON, -j JSON
+                        Name of a project to decode, eg. Sample
+  --markdown, -t        Print a markdown table of all Parameter IDs
+  --midi MIDI, -m MIDI  Enable MIDI export and provide path to output file, eg. Sample.mid
+  --json JSON, -j JSON  Enable JSON dump of decoded keys and provide path to output file, eg. Sample.json
 ```
 
-- **-i/--instrument** looks for Instrument information in the `Instruments` folder, defaults to "KeyStepPro"
-- **-p/--project** looks for a project with the name you specify file with the specified instrument as the file extension in the `Projects` folder, eg. "Sample" looks for `Projects/Sample.KeyStepPro`.
-- **-m/--midi** is the file path to your exported MIDI file, defaults to `Sample.mid`
-- **-j/--json** is the file path to a JSON file which is the decoded project file
-
-## Example
+## Example of Full Usage
 
 ```
-% python main.py -i KeyStepPro -p Sample -m Sample.mid -j Sample.json
-Detected Tempo: 120.0
+% python main.py -d KeyStepPro -p Sample -m Sample.mid -j Sample.json -t
+Reading project file Projects/Sample.KeyStepPro
+Reading parameters file Devices/KeyStepPro.json
+Detected Tempo: 120.00
+Converting Track 1 to MIDI
+Patterns in track1: [1]
+Converting Track 2 to MIDI
+Patterns in track2: [1]
+Converting Track 3 to MIDI
+Patterns in track3: [1]
+Converting Track 4 to MIDI
+Patterns in track4: [1]
 MIDI written to Sample.mid
+Decoded project written to Sample.json
 ```
 
 ## To Do
 - Support other Arturia devices
 - Support for MIDI conversion to Project file
 - Support for more parameters, eventually getting all of them.
+
+# KeyStep Pro Data
+
+The following is just me doing my best to interpret the data in the `KeyStepPro.json` file against a `*.KeyStepPro` project file. My current understanding is incomplete.
+
+## Key Format
+
+Keys in *.KeyStepPro files follow the pattern of `(Track)_(Parameter)_(Pattern)_(Unknown)_(Note)`. The purpose of the fourth slot in the key name is currently uknown to me, but always has three copies.
+
+For example, the key `124_111_16_1_32` corresponds to the following:
+
+| Parameter | Description |
+|-------|-----|
+| 124 | Track 2 | 
+| 111 | Seq note velocity (See table below)
+| 16 | Pattern 16
+| 1 | Unknown |
+| 32 | Note/Step 32 |
+
+## Tracks 
+
+| ID | Track |
+|-------|-----|
+| 120 | Project Settings |
+| 121 | State Settings |
+| 122 | Control Settings |
+| 123 | Track 1 SEQ or DRUM |
+| 124 | Track 2 SEQ or ARP |
+| 125 | Track 3 SEQ or ARP |
+| 126 | Track 4 SEQ or ARP |
+
+## Parameters
+| ID | Description |
+|-----|-----|
+| 20 | Pattern Seq Program Change Value (Program Change Value (4 LSB) : bit 0 - 3) |
+| 21 | Pattern Seq Program Change Value (Write flag : bit 6) |
+| 22 | Pattern Seq Program Change Value MSB (Program Change Value MSB) |
+| 23 | Pattern Seq Program Change Value LSB (Program Change Value LSB) |
+| 25 | Pattern Drum Program Change Value (Program Change Value (4 LSB) : bit 0 - 3) |
+| 26 | Pattern Drum Program Change Value (Write flag : bit 6) |
+| 27 | Pattern Drum Program Change Value MSB (Program Change Value MSB) |
+| 28 | Pattern Drum Program Change Value LSB (Program Change Value LSB) |
+| 37 | Project data state (Project data state) |
+| 39 | Track data state (Track data state) |
+| 40 | Pattern data state (Pattern data state) |
+| 48 | Step Seq step active |
+| 49 | Step Seq step skip |
+| 50 | Seq note corresponding step |
+| 51 | DRUM poly step count |
+| 52 | DRUM step active |
+| 53 | DRUM step skip |
+| 54 | DRUM step corresponding step |
+| 55 | Track tranpose group (Is track in transpose group) |
+| 56 | ARP swing (ARP swing) |
+| 59 | Track seq color |
+| 60 | Track drum color |
+| 68 | ARP group 1 parameters (ARP track 3 size : bit 5 - 6) |
+| 69 | ARP group 2 parameters (ARP track 4 size : bit 1 - 2) |
+| 70 | Project tempo part 3 (LSB) (Tempo is divided into 3 part (MSB - MidSB - LSB)) |
+| 71 | Project tempo part 2 (MidSB) (Tempo is divided into 3 part (MSB - MidSB - LSB)) |
+| 73 | global BPM, metronome triplet, metronome size, wait to load in a bitfield (wait to load : bit 4) |
+| 75 | Current scene |
+| 85 | Track transposition state, transposition octave in a bitfield (transposition octave : bit 4 - 5 - 6) |
+| 86 | Track keyboard octave, chord mode state, Arp/Drum mode state in a bitfield (Arp/Drum mode state : bit 6) |
+| 87 | Chord |
+| 97 | Pattern Seq swing (%) (an offset of 25 is applied to be send by MIDI) (-25% to +25%) |
+| 98 | Pattern Seq step count |
+| 99 | Pattern Seq triplet state, swing offset state, polyrythm state, step size, playback direction in a bitfield (playback direction : bit 5 - 6) |
+| 100 | Pattern Seq ARP/Drum mode, ARP type, ARP octave in a bitfield (ARP octave : bit 4 - 5 - 6) |
+| 101 | Pattern Seq user scale 1 part 3 (LSB) (User scale 1 is divided into 3 part (MSB - MidSB - LSB)) |
+| 102 | Pattern Seq user scale 1 part 2 (MidSB) (User scale 1 is divided into 3 part (MSB - MidSB - LSB)) |
+| 103 | Pattern Seq user scale 1 part 1 (MSB) (User scale 1 is divided into 3 part (MSB - MidSB - LSB)) |
+| 104 | Pattern Seq user scale 2 part 3 (LSB) (User scale 2 is divided into 3 part (MSB - MidSB - LSB)) |
+| 105 | Pattern Seq user scale 2 part 2 (MidSB) (User scale 2 is divided into 3 part (MSB - MidSB - LSB)) |
+| 106 | Pattern Seq user scale 2 part 1 (MSB) (User scale 2 is divided into 3 part (MSB - MidSB - LSB)) |
+| 107 | Pattern Seq root note |
+| 108 | Pattern Seq scale |
+| 109 | Seq note pitch |
+| 110 | Seq note gate length |
+| 111 | Seq note velocity |
+| 112 | Seq note time shift |
+| 113 | Seq note randomness |
+| 114 | Pattern DRUM swing (%) (an offset of 25 is applied to be send by MIDI) (-25% to +25%) |
+| 115 | Pattern DRUM step count |
+| 116 | Pattern DRUM triplet state, swing offset state, polyrythm state, step size, playback direction in a bitfield (playback direction : bit 5 - 6) |
+| 117 | DRUM note pitch |
+| 118 | DRUM note gate length |
+| 119 | DRUM note velocity |
+| 120 | DRUM note time shift |
+| 121 | DRUM note randomness |
+| 122 | Track transposition |
+| 123 | Track MIDI channel (Track MIDI channel of the project) |
